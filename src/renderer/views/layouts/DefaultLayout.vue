1a3973f8053299a851f623704fbe050042f7fa68
Josseth Arroyo <55711477+cheche482010@users.noreply.github.com>
2025-10-03 10:54:31 -0400

    solcion warn

--- src/renderer/views/layouts/DefaultLayout.vue
+++ src/renderer/views/layouts/DefaultLayout.vue
@@ -2,28 +2,18 @@
     <v-app>
         <v-navigation-drawer v-model="drawer" app>
             <v-list-item>
-                <v-list-item-content>
-                    <v-list-item-title class="text-h6">
-                        Facturación
-                    </v-list-item-title>
-                    <v-list-item-subtitle>
-                        Menú Principal
-                    </v-list-item-subtitle>
-                </v-list-item-content>
+                <v-list-item-title class="text-h6">
+                    Facturación
+                </v-list-item-title>
+                <v-list-item-subtitle>
+                    Menú Principal
+                </v-list-item-subtitle>
             </v-list-item>
 
             <v-divider></v-divider>
 
             <v-list dense nav>
-                <v-list-item v-for="item in menuItems" :key="item.title" :to="item.to" link>
-                    <v-list-item-icon>
-                        <v-icon>{{ item.icon }}</v-icon>
-                    </v-list-item-icon>
-
-                    <v-list-item-content>
-                        <v-list-item-title>{{ item.title }}</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
+                <v-list-item v-for="item in menuItems" :key="item.title" :to="item.to" link :prepend-icon="item.icon" :title="item.title" />
             </v-list>
 
             <template v-slot:append>
