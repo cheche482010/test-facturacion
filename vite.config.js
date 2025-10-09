16043ac49f8462feffb589bedee9d3b1fb280b7c
Josseth Arroyo <55711477+cheche482010@users.noreply.github.com>
2025-10-09 14:58:54 -0400

    error load

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -14,8 +13,8 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      // Redirigir las peticiones de /api al servidor backend en el puerto 3001
       '/api': 'http://localhost:3001'
     }
-  }
+  },
+  base: './'
 })
